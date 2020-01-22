ee50113b104fcb683e06924336864f87edef4967
Priya <pvaidya56@gmail.com>
2020-01-22 02:23:35 -0800

    create useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,15 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (bool) => {
+    const [mode, setMode] = useLocalStorage("darkMode", bool);
+  
+    React.useEffect(() => {
+      window.localStorage.getItem("darkMode") === "true"
+        ? document.querySelector("body").classList.add("dark-mode")
+        : document.querySelector("body").classList.remove("dark-mode");
+    }, [mode]);
+  
+    return [mode, setMode];
+  };
+  
+  export default useDarkMode;
